38c80fc953bbb5042c37946342310e74cc80f982
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2022-03-02 19:40:52 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyD0WaNN6xoYjW1WAc3xswpCK_nS1_pF7jQ",
+    authDomain: "test-ee698.firebaseapp.com",
+    projectId: "test-ee698",
+    storageBucket: "test-ee698.appspot.com",
+    messagingSenderId: "342187310937",
+    appId: "1:342187310937:web:18a0b2e099a6209b6e5648"
+  };
+  
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
